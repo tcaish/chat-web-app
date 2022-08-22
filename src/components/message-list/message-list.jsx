b31b95cb77a317a1dbba34aad79b1a6f7fe215bc
tcaish <tim.caish@foolwealth.com>
2022-08-22 18:05:05 -0400

    fixed things

---
+++ src/components/message-list/message-list.jsx
@@ -0,0 +1,7 @@
+import './message-list.scss';
+
+function MessageList() {
+  return <div className="card-background">Message List</div>;
+}
+
+export default MessageList;
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,5 +1,20 @@
+import { Col, Row } from 'react-bootstrap';
+import MessageList from '../../components/message-list/message-list';
+import './home.scss';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <Row>
+        <Col lg={4}>
+          <MessageList />
+        </Col>
+        <Col>
+          <div className="card-background">Messsage</div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Home;
