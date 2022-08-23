a52d90f105070417ca20b72d2659ee4785457816
tcaish <tim.caish@foolwealth.com>
2022-08-23 07:51:41 -0400

    changed background; added search bar to message list

--- src/components/message-list/message-list.jsx
+++ src/components/message-list/message-list.jsx
@@ -1,7 +1,12 @@
+import SearchBar from '../search-bar/search-bar';
 import './message-list.scss';
 
 function MessageList() {
-  return <div className="card-background">Message List</div>;
+  return (
+    <div className="card-background">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default MessageList;
---
+++ src/components/search-bar/search-bar.jsx
@@ -0,0 +1,22 @@
+import { Form, InputGroup } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs';
+import './search-bar.scss';
+
+function SearchBar() {
+  return (
+    <div className="search-bar-container">
+      <InputGroup>
+        <InputGroup.Text className="search-bar-input-icon">
+          <BsSearch />
+        </InputGroup.Text>
+        <Form.Control
+          className="search-bar-input"
+          placeholder="Search..."
+          aria-label="Search"
+        />
+      </InputGroup>
+    </div>
+  );
+}
+
+export default SearchBar;
