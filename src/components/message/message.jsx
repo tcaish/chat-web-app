d4f9fe349bf7520ec26c6fe07698d40c76b9d8ed
tcaish <tim.caish@foolwealth.com>
2022-08-26 08:13:17 -0400

    messages now showing based on who sent it; messages styled appropriately

--- src/components/message/message.jsx
+++ src/components/message/message.jsx
@@ -1,32 +1,56 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUsersInfo } from '../../redux/slices/messagesSlice';
+import { selectUser } from '../../redux/slices/userSlice';
 import './message.scss';
 
 function Message({ message }) {
+  const user = useSelector(selectUser);
   const usersInfo = useSelector(selectUsersInfo);
 
   const [photoURL, setPhotoURL] = useState('');
+  const [isUsersMessage, setIsUsersMessage] = useState(false);
 
   // Sets the photo URL for the sender of the message
+  // Sets the type of message to display (user's or other user's)
   useEffect(() => {
     if (usersInfo) {
-      const user = usersInfo.filter((u) => u.uid === message.sender)[0];
-      setPhotoURL(user.photo_url);
+      const sender = usersInfo.filter((u) => u.uid === message.sender)[0];
+      setPhotoURL(sender.photo_url);
+      setIsUsersMessage(sender.uid === user.uid);
     }
-  }, [message.sender, usersInfo]);
+  }, [user, message.sender, usersInfo]);
 
-  return (
-    <div className="message-container">
+  return isUsersMessage ? (
+    <div className="message-container message-container-left-aligned">
       {photoURL && (
         <>
           <div className="message-image">
             <img src={photoURL} alt="profile" />
           </div>
 
-          <div className="message-content">
+          <div className="message-content message-content-left-aligned">
             {message.message}
-            <span>{message.getFormattedDateForMessage()}</span>
+            <span className="message-content-time message-content-time-left-aligned">
+              {message.getFormattedDateForMessage()}
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  ) : (
+    <div className="message-container message-container-right-aligned">
+      {photoURL && (
+        <>
+          <div className="message-content message-content-right-aligned">
+            {message.message}
+            <span className="message-content-time message-content-time-right-aligned">
+              {message.getFormattedDateForMessage()}
+            </span>
+          </div>
+
+          <div className="message-image">
+            <img src={photoURL} alt="profile" />
           </div>
         </>
       )}
