1d77e739306c1b8a4e972f3d6c6ec3ffc5ce249f
tcaish <tim.caish@foolwealth.com>
2022-08-29 08:23:05 -0400

    shift + enter to send message

--- src/components/message-view-footer/message-view-footer.jsx
+++ src/components/message-view-footer/message-view-footer.jsx
@@ -1,4 +1,4 @@
-import { Spinner, Textarea } from 'evergreen-ui';
+import { Badge, Spinner, Textarea } from 'evergreen-ui';
 import { IoMdSend } from 'react-icons/io';
 import './message-view-footer.scss';
 
@@ -8,18 +8,6 @@ function MessageViewFooter({
   sendMessage,
   sendingMessage
 }) {
-  // const handleShiftEnter = (event) => {
-  //   if (event.keyCode === 13 && event.shiftKey) {
-  //     if (event.type === 'keydown') {
-  //       sendMessage(inputText);
-  //     }
-  //   }
-  // };
-
-  // const textarea = document.querySelector('textarea');
-  // console.log(textarea);
-  // textarea.addEventListener('keydown', handleShiftEnter);
-
   return (
     <div className="message-view-footer-container">
       <div className="message-view-footer-input-container">
@@ -29,6 +17,14 @@ function MessageViewFooter({
           disabled={sendingMessage}
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(event) => {
+            // Send message when SHIFT + ENTER pressed
+            if (event.keyCode === 13 && event.shiftKey) {
+              if (event.type === 'keydown') {
+                sendMessage(inputText);
+              }
+            }
+          }}
         />
         <div
           className="message-view-footer-send-container"
@@ -38,6 +34,18 @@ function MessageViewFooter({
             {!sendingMessage ? <IoMdSend /> : <Spinner size={24} />}
           </span>
         </div>
+      </div>
+      <div className="message-view-footer-message-container">
+        <p>
+          <Badge color="neutral" marginRight={6}>
+            Shift
+          </Badge>
+          +
+          <Badge color="neutral" marginLeft={6} marginRight={6}>
+            Enter
+          </Badge>
+          to send message
+        </p>
       </div>
     </div>
   );
