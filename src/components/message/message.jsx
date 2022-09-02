81f0b4b8b46c2f80cddfd72eede6f8bdb4b17e76
tcaish <tim.caish@foolwealth.com>
2022-09-02 09:31:18 -0400

    updated message in conversation to use avatar component for profile picture instead of <img/>

--- src/components/message/message.jsx
+++ src/components/message/message.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from 'evergreen-ui';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUsersInfo } from '../../redux/slices/messagesSlice';
@@ -8,6 +9,7 @@ function Message({ message }) {
   const user = useSelector(selectUser);
   const usersInfo = useSelector(selectUsersInfo);
 
+  const [displayName, setDisplayName] = useState('');
   const [photoURL, setPhotoURL] = useState('');
   const [isUsersMessage, setIsUsersMessage] = useState(false);
 
@@ -16,6 +18,7 @@ function Message({ message }) {
   useEffect(() => {
     if (usersInfo) {
       const sender = usersInfo.filter((u) => u.uid === message.sender)[0];
+      setDisplayName(sender.display_name);
       setPhotoURL(sender.photo_url);
       setIsUsersMessage(sender.uid === user.uid);
     }
@@ -23,37 +26,39 @@ function Message({ message }) {
 
   return !isUsersMessage ? (
     <div className="message-container message-container-left-aligned">
-      {photoURL && (
-        <>
-          <div className="message-image">
-            <img src={photoURL} alt="profile" />
-          </div>
+      <div className="message-avatar">
+        <Avatar
+          size={40}
+          referrerPolicy="no-referrer"
+          name={displayName ? displayName : ''}
+          src={photoURL ? photoURL : ''}
+        />
+      </div>
 
-          <div className="message-content message-content-left-aligned">
-            {message.message}
-            <span className="message-content-time message-content-time-left-aligned">
-              {message.getFormattedDateForMessage()}
-            </span>
-          </div>
-        </>
-      )}
+      <div className="message-content message-content-left-aligned">
+        {message.message}
+        <span className="message-content-time message-content-time-left-aligned">
+          {message.getFormattedDateForMessage()}
+        </span>
+      </div>
     </div>
   ) : (
     <div className="message-container message-container-right-aligned">
-      {photoURL && (
-        <>
-          <div className="message-content message-content-right-aligned">
-            {message.message}
-            <span className="message-content-time message-content-time-right-aligned">
-              {message.getFormattedDateForMessage()}
-            </span>
-          </div>
+      <div className="message-content message-content-right-aligned">
+        {message.message}
+        <span className="message-content-time message-content-time-right-aligned">
+          {message.getFormattedDateForMessage()}
+        </span>
+      </div>
 
-          <div className="message-image">
-            <img src={photoURL} alt="profile" />
-          </div>
-        </>
-      )}
+      <div className="message-avatar">
+        <Avatar
+          size={40}
+          referrerPolicy="no-referrer"
+          name={displayName ? displayName : ''}
+          src={photoURL ? photoURL : ''}
+        />
+      </div>
     </div>
   );
 }
